e5cb1460e61367f6a239da233103d1a9a74ba2b2
PhoenixGS <thestarrydream@gmail.com>
2019-04-23 12:01:03 +0800

    更新SA模版

--- String/SA.cpp
+++ String/SA.cpp
@@ -1,94 +1,92 @@
 #include <cstdio>
 #include <cstring>
+#include <algorithm>
 
-int T;
-char s[2000];
-int v[2000];
-int sa[2][2000], rank[2][2000];
-int* SA;
-int* RANK;
-int h[2000];
-int n;
-int k;
-int ans;
+const int _n = 100000 + 10;
+int n, m;
+char s[_n];
+int sa[_n], rk[_n], tp[_n], h[_n], cnt[_n];
 
-void calcsa(int* sa, int* rank, int* SA, int* RANK)
+void rsort()
 {
-	rank[n + 1] = 0;
+	for (int i = 1; i <= m; i++)
+	{
+		cnt[i] = 0;
+	}
 	for (int i = 1; i <= n; i++)
 	{
-		v[rank[sa[i]]] = i;
+		cnt[rk[i]]++;
+	}
+	for (int i = 1; i <= m; i++)
+	{
+		cnt[i] += cnt[i - 1];
 	}
 	for (int i = n; i >= 1; i--)
 	{
-		if (sa[i] > k)
+		sa[cnt[rk[tp[i]]]--] = tp[i];
+	}
+}
+
+void ssort()
+{
+	for (int i = 1; i <= n; i++)
+	{
+		rk[i] = s[i];
+		tp[i] = i;
+	}
+	rsort();
+	for (int w = 1, p = 0; p < n && w <= n; m = p, w <<= 1)
+	{
+		p = 0;
+		for (int i = n - w + 1; i <= n; i++)
 		{
-			SA[v[rank[sa[i] - k]]--] = sa[i] - k;
+			tp[++p] = i;
 		}
-	}
-	for (int i = n - k + 1; i <= n; i++)
-	{
-		SA[v[rank[i]]--] = i;
-	}
-	RANK[SA[1]] = 1;
-	for (int i = 2; i <= n; i++)
-	{
-		RANK[SA[i]] = RANK[SA[i - 1]] + (rank[SA[i]] != rank[SA[i - 1]] || rank[SA[i] + k] != rank[SA[i - 1] + k]);
-	}
-}
-
-void getsa()
-{
-	int p = 0, q = 1;
-	memset(v, 0, sizeof(v));
-	for (int i = 1; i <= n; i++)
-	{
-		v[x[i]]++;
-	}
-	for (int i = 1; i <= 128; i++)
-	{
-		v[i] = v[i - 1] + v[i];
-	}
-	for (int i = 1; i <= n; i++)
-	{
-		sa[p][v[s[i]]--] = i;
-	}
-	rank[p][sa[p][1]] = 1;
-	for (int i = 2; i <= n; i++)
-	{
-		rank[p][sa[p][i]] = rank[p][sa[p][i - 1]] + (s[sa[p][i]] != s[sa[p][i - 1]]);
-	}
-	for (k = 1; k < n; k <<= 1, p ^= 1, q ^= 1)
-	{
-		calcsa(sa[p], rank[p], sa[q], rank[q]);
-	}
-	SA = (sa[p]);
-	RANK = (rank[p]);
-}
-
-void geth()
-{
-	for (int i = 1, j, k = 0; i <= n; h[RANK[i++]] = k)
-	{
-		for (k = k ? k - 1 : k, j = SA[RANK[i] - 1]; s[i + k] == s[j + k]; k++);
+		for (int i = 1; i <= n; i++)
+		{
+			if (sa[i] > w)
+			{
+				tp[++p] = sa[i] - w;
+			}
+		}
+		rsort();
+		std::swap(rk, tp);
+		rk[sa[1]] = p = 1;
+		for (int i = 2; i <= n; i++)
+		{
+			if (tp[sa[i]] == tp[sa[i - 1]] && tp[std::min(n + 1, sa[i] + w)] == tp[std::min(n + 1, sa[i - 1] + w)])
+			{
+				rk[sa[i]] = p;
+			}
+			else
+			{
+				rk[sa[i]] = ++p;
+			}
+		}
 	}
 }
 
 int main()
 {
-	scanf("%d", &T);
-	while (T--)
+	scanf("%s", s + 1);
+	n = strlen(s + 1);
+	m = 127;
+	ssort();
+	for (int i = 1, j, k = 0; i <= n; i++)
 	{
-		scanf("%s", s + 1);
-		n = strlen(s + 1);
-		getsa();
-		geth();
-		ans = n * (n + 1) / 2;
-		for (int i = 1; i <= n; i++)
-		{
-			ans = ans - h[i];
-		}
-		printf("%d\n", ans);
+		for (k = k ? k - 1 : k, j = sa[rk[i] - 1]; s[i + k] == s[j + k]; k++);
+		h[rk[i]] = k;
 	}
+	for (int i = 1; i <= n; i++)
+	{
+		printf("%d ", sa[i]);
+	}
+	puts("");
+	for (int i = 2; i <= n; i++)
+	{
+		printf("%d ", h[i]);
+	}
+	puts("");
 	return 0;
 }
+
